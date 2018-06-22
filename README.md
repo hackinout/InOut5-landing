cea2256944b042dfe062cc63f85d7b4fda9a3575
Abhishek Warokar <abhishek11@live.in>
2018-06-22 20:29:40 +0530

    :tada: InOut 5 init

---
+++ README.md
@@ -0,0 +1,9 @@
+# InOut: India's leading Hackathon
+Main site of InOut 5 [WIP]
+
+## How to run:
+
+1. Clone the repo.
+2. Run `yarn`.
+3. Run `yarn start` to watch.
+4. Run `yarn build` to build locally. Check `public` directory for the files.
---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+'use strtict'
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var reload = browserSync.reload
+var concat = require('gulp-concat')
+var minify = require('gulp-minify')
+var cleanCss = require('gulp-clean-css')
+var rename = require('gulp-rename')
+var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer')
+var htmlmin = require('gulp-htmlmin')
+var del = require('del')
+var uncss = require('gulp-uncss')
+var uglify = require('gulp-uglify')
+var pump = require('pump')
+var sass = require('gulp-sass')
+var svgSprite = require('gulp-svg-sprite')
+// Default Gulp task to run including all necessary dependencies
+gulp.task('default', ['browser-sync', 'build'], function () {
+  gulp.watch(['source/**/*.html', 'source/js/*.js',
+    'source/scss/**/*.scss'
+  ], ['build'])
+  gulp.watch(['public/**/*.html', 'public/js/*.js',
+    'public/css/*.css'
+  ], reload)
+})
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: './public'
+  })
+})
+
+// Build task to initiate minify tasks for CSS and JS
+gulp.task('build', ['copy-favicons', 'minify-html', 'pack-minify-js', 'sass', 'gulp-uncss', 'copy-img', 'copy-css', 'copy-pages'
+])
+
+// Task to minify HTML
+gulp.task('minify-html', function () {
+  return gulp.src(['source/*.html', 'source/*/*.html'])
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('public/'))
+})
+
+// Task to uglify JS
+gulp.task('pack-minify-js', function (cb) {
+  pump([
+    gulp.src(['source/js/*.js', '!source/js/*.min.js'])
+      .pipe(minify({
+        ext: {
+          min: '.min.js'
+        },
+        noSource: true
+      })),
+    uglify(),
+    gulp.dest('./public/js')
+  ],
+    cb
+  )
+})
+
+// sass
+gulp.task('sass', function () {
+  var plugins = [
+    autoprefixer({ browsers: ['last 1 version'] })
+  ]
+  return gulp.src('./source/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('./public/css'))
+})
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('.source/scss/**/*.scss', ['sass'])
+// })
+
+// Task to minify CSS
+// gulp.task('pack-minify-css', function () {
+//   return gulp.src(['source/css/*.css', '!source/css/*.min.css'])
+//     .pipe(concat('main.css'))
+//     .pipe(cleanCss())
+//     .pipe(rename({
+//       suffix: '.min'
+//     }))
+//     .pipe(gulp.dest('public/css'))
+// })
+
+// Task to remove unused css
+gulp.task('gulp-uncss', function () {
+  return gulp.src('./css/bootstrap-custom.css')
+    .pipe(uncss({
+      html: ['index.html'],
+      ignore: [/\modal/]
+    }))
+    .pipe(gulp.dest('public/css'))
+})
+
+// Task to copy assets
+gulp.task('copy-img', function () {
+  return gulp.src('source/assets/img/**/*')
+    .pipe(gulp.dest('public/assets/img'))
+})
+
+gulp.task('copy-pages', function () {
+  return gulp.src(['source/*/*.html', 'source/*/*.min.css', 'source/*/*.min.js'])
+    .pipe(gulp.dest('public/'))
+})
+
+// Task to copy faviocons
+gulp.task('copy-favicons', function () {
+  return gulp.src(['source/*.png', 'source/*.ico', 'source/*.xml', 'source/*.json', '!source/index.html'])
+    .pipe(gulp.dest('public/'))
+})
+
+gulp.task('copy-css', function () {
+  return gulp.src('source/css/*.min.css')
+    .pipe(gulp.dest('public/css'))
+})
+
+// Task to delete target assets folder for recreation
+gulp.task('clean', function () {
+  return del(['public/**', '!public'])
+})
+
+var config = {
+  mode: {
+    css: {
+      render: {
+        css: true
+      }
+    }
+  }
+}
+
+gulp.task('svgSprite', function () {
+  return gulp.src('source/assets/img/icons/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('public/css'))
+})
