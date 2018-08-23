1f88b1825a2b4e962faae240d18c55e2a00bc1a2
ajayns <ajay.ns@outlook.com>
2018-08-23 21:39:26 +0530

    :sparkles: Hero animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Shrink navbar on scroll
-  $(window).scroll(function(event) {
-    var scroll = $(window).scrollTop();
-    if (scroll > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  });
-
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (
@@ -42,6 +32,86 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $("body").scrollspy({
     target: "#mainNav",
-    offset: 57
+    offset: 56
+  });
+
+  // Header scroll animations
+
+  // function setPeriodicTransform(maxVal, val) {
+  // Get periodic value
+  // Varies from 0 to maxVal periodically
+  // 42 * maxVal is damping factor
+  // Why 42? Cause it's the answer to life, universe and everything
+  //  return Math.abs(maxVal * Math.sin(val / (42 * maxVal)));
+  //}
+
+  var viewHeight = $(window).height();
+  $(window).scroll(function(event) {
+    var scrollPos = $(window).scrollTop();
+
+    function getTranslateVal(maxVal) {
+      // Animation completes in 1/4 of screen height scroll
+      var heightFrac = viewHeight / 4;
+      return maxVal * (Math.min(heightFrac, scrollPos) / heightFrac);
+    }
+
+    $("#heroImage>.c2").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(3) +
+        "vw," +
+        getTranslateVal(2.5) +
+        "vw, 0)"
+    );
+    $("#heroImage>.c3").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(3) +
+        "vw," +
+        getTranslateVal(2.5) +
+        "vw, 0)"
+    );
+    $("#heroImage>.c4").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(-3) +
+        "vw," +
+        getTranslateVal(-2.5) +
+        "vw, 0)"
+    );
+
+    $("#heroImage>.b1").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(4) +
+        "vw," +
+        getTranslateVal(3) +
+        "vw, 0)"
+    );
+    $("#heroImage>.b2").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(-3) +
+        "vw," +
+        getTranslateVal(1.5) +
+        "vw, 0)"
+    );
+
+    $("#heroImage>.v1").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(4) +
+        "vw," +
+        getTranslateVal(-1.5) +
+        "vw, 0)"
+    );
+    $("#heroImage>.v2").css(
+      "transform",
+      "translate3d(" +
+        getTranslateVal(4) +
+        "vw," +
+        getTranslateVal(-1.5) +
+        "vw, 0)"
+    );
   });
 })(jQuery); // End of use strict
